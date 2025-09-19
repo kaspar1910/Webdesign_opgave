5721bf0ec41987473c3f44a21e2395c1b6082589
kaspar1910 <kaspar1910@gmail.com>
2025-09-20 01:15:54 +0200

    background animation, button optimization and animation and layout changes

--- Garden_website_gg/obj/Debug/net9.0/Garden_website_gg.AssemblyInfo.cs
+++ Garden_website_gg/obj/Debug/net9.0/Garden_website_gg.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Garden_website_gg")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bdfa7467aa8e7c0494734e5eea175258e279ae7c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd10179ccd204f9a454e4c670ec80169961fee50")]
 [assembly: System.Reflection.AssemblyProductAttribute("Garden_website_gg")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Garden_website_gg")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
