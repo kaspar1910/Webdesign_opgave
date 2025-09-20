c97b2088394b46acbbed29416d4e8d636b061de1
kaspar1910 <kaspar1910@gmail.com>
2025-09-20 10:22:17 +0200

    background update

--- Garden_website_gg/obj/Debug/net9.0/Garden_website_gg.AssemblyInfo.cs
+++ Garden_website_gg/obj/Debug/net9.0/Garden_website_gg.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Garden_website_gg")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd10179ccd204f9a454e4c670ec80169961fee50")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5721bf0ec41987473c3f44a21e2395c1b6082589")]
 [assembly: System.Reflection.AssemblyProductAttribute("Garden_website_gg")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Garden_website_gg")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
