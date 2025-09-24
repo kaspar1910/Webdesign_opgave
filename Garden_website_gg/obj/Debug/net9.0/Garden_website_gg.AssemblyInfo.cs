66b0b020615e0f4311d2084d18b3ec43f4eba89e
kaspar1910 <kaspar1910@gmail.com>
2025-09-24 20:10:05 +0200

    upload page progress

--- Garden_website_gg/obj/Debug/net9.0/Garden_website_gg.AssemblyInfo.cs
+++ Garden_website_gg/obj/Debug/net9.0/Garden_website_gg.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Garden_website_gg")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bf4d2ff340eeaf043e117b1bd8a96b560696661")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1698a038cbc0f708fdf28ce2ab9389a3b94c4164")]
 [assembly: System.Reflection.AssemblyProductAttribute("Garden_website_gg")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Garden_website_gg")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
