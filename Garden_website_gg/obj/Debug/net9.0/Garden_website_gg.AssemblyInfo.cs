aa760a0b1c15f835c2f51fe58fe3ebad2c3a04a6
Niels-Just <nison24@student.sdu.dk>
2025-10-08 11:19:50 +0200

    Mac specifc

--- Garden_website_gg/obj/Debug/net9.0/Garden_website_gg.AssemblyInfo.cs
+++ Garden_website_gg/obj/Debug/net9.0/Garden_website_gg.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Garden_website_gg")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+839b3cea85aa3bca83eccc4fb0463eab27efe929")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15c66e480b8eb031460ae5fb2289e0b83c428b23")]
 [assembly: System.Reflection.AssemblyProductAttribute("Garden_website_gg")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Garden_website_gg")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
