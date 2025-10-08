db4a1a1a84e23d8d9a75ce89a8ccde83f46782ad
Niels-Just <nison24@student.sdu.dk>
2025-10-08 12:23:20 +0200

    Larvel convertion

--- Garden_website_gg/obj/Debug/net9.0/Garden_website_gg.AssemblyInfo.cs
+++ Garden_website_gg/obj/Debug/net9.0/Garden_website_gg.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Garden_website_gg")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15c66e480b8eb031460ae5fb2289e0b83c428b23")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34996ce5653ae49356db7a725966f2eaf7b93b44")]
 [assembly: System.Reflection.AssemblyProductAttribute("Garden_website_gg")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Garden_website_gg")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
